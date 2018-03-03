420a136140b6974e559783616d0e1943f8e354d4
concourse-tmp <36944892+concourse-tmp@users.noreply.github.com>
2018-03-03 15:07:58 +0000

    Update test.sh

--- git-pull/test.sh
+++ git-pull/test.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
 set -e -x -u
 
-echo "hello world"
-
-#if [[ $value -gt 16384 ]]; then
-#    cat > notify_message/message <<EOF
-#job 'test' failed. Random value $value need to be less than 16384 to be succeed.
-#EOF
-#    exit 1
-#else
-#    cat  > notify_message/message <<EOF
-#job 'test' succeeded. Random value $value is less than 16384
-#EOF
-#    exit 0
-#fi
+echo "1"
